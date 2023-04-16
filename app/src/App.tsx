801712e849ad20b383d11094395cf4e4638b75e6
Ussf-nassrallah <nassrallah.dev@gmail.com>
2023-04-16 21:34:12 +0000

    passing props to InputFeild Component

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,16 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
 // COMPONENTS
 import InputFeild from './Components/InputFeild';
 
 const App: React.FC = () => {
+  const [todo, setTodo] = useState<string>("");
+  console.log(todo);
+
   return (
     <div className="App">
       <div className="App-container">
         <header className='App-header'>Todo App</header>
-        
-        <InputFeild />
+        <InputFeild todo={todo} setTodo={setTodo} />
       </div>
     </div>
   );
--- app/src/Components/InputFeild.tsx
+++ app/src/Components/InputFeild.tsx
@@ -1,10 +1,21 @@
 import React from 'react';
 import "./InputFeild.css";
 
-const InputFeild: React.FC = () => {
+interface Props {
+  todo: string,
+  setTodo: React.Dispatch<React.SetStateAction<string>>
+}
+
+const InputFeild: React.FC<Props> = ({ todo, setTodo }) => {
   return (
     <form className='input'>
-      <input type="text" placeholder='Enter a Task' className='input__box' />
+      <input
+        value={todo}
+        onChange={(e) => setTodo(e.target.value)}
+        type="text"
+        placeholder='Enter a Task'
+        className='input__box'
+      />
       <button type='submit' className='input__submit'>Submit</button>
     </form>
   )
