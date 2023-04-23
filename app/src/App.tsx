4344bedf40b81bc7a6bb20695f30e6b47a6fb9c5
Ussf-nassrallah <nassrallah.dev@gmail.com>
2023-04-23 12:17:16 +0100

    create Todo Logic

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { Todo } from './Models/model';
 import './App.css';
 
 // COMPONENTS
@@ -6,13 +7,24 @@ import InputFeild from './Components/InputFeild';
 
 const App: React.FC = () => {
   const [todo, setTodo] = useState<string>("");
-  console.log(todo);
+  const [todos, setTodos] = useState<Todo[]>([]);
+  
+  const handleAdd = (e: React.FormEvent) => {
+    e.preventDefault();
+
+    if (todo) {
+      setTodos([...todos, { id: Date.now(), todo, isDone: false }]);
+      setTodo("");
+    }
+  }
+
+  console.log(todos);
 
   return (
     <div className="App">
       <div className="App-container">
         <header className='App-header'>Todo App</header>
-        <InputFeild todo={todo} setTodo={setTodo} />
+        <InputFeild todo={todo} setTodo={setTodo} handleAdd={handleAdd} />
       </div>
     </div>
   );
